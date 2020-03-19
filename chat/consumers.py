5426b12ab3b1d7a9632e28bab21e9e909c9732bf
TWESIGOMWE GILBERT <gilbert.gillz@gmail.com>
2020-03-19 18:31:01 +0300

    save websocket message in database, live chat working on django

--- chat/consumers.py
+++ chat/consumers.py
@@ -9,12 +9,61 @@ Every consumer instance has an automatically generated unique channel name, and 
 
 In our chat application we want to have multiple instances of ChatConsumer in the same room communicate with each other. To do that we will have each ChatConsumer add its channel to a group whose name is based on the room name. That will allow ChatConsumers to transmit messages to all other ChatConsumers in the same room.
 '''
-
+from django.contrib.auth import get_user_model
 import json
 from asgiref.sync import async_to_sync
 from channels.generic.websocket import WebsocketConsumer
+from chat.models import Message
+# from django.contrib.auth.models import User
+
+User = get_user_model()
 
 class ChatConsumer(WebsocketConsumer):
+    commands = {
+        'fetch_messages': 'fetch_messages',
+        'new_message': 'new_message'
+    }
+
+    def fetch_messages(self, data):
+        messages = Message.objects.all()
+        # Message.last_10_messages()
+        content = {
+            'command': 'messages',
+            'chat_messages': self.messages_to_json(messages)
+        }
+        # print("the messages retrieved: {}".format(messages))
+        self.send_message(content)
+
+    def messages_to_json(self, messages):
+        result = []
+        for message in messages:
+            result.append(self.message_to_json(message))
+            print("Result : {}".format(self.message_to_json(message)))
+        return result
+
+    def message_to_json(self, message):
+        return {
+            'author': message.author.username,
+            'content': message.content,
+            'timestamp': str(message.timestamp)
+        }
+
+    def new_message(self, data):
+        author = data['from']
+        author_user = User.objects.get(username=author)
+        print("the user: {}".format(author_user.username))
+        message = Message.objects.create(
+            author = author_user,
+            content = data['message']
+        )
+        print("author: {} data: {}".format(author, data))
+        # print("the message : {}".format(message))
+        content = {
+            'command': 'new_message',
+            'chat_message': self.message_to_json(message)
+        }
+        return self.send_chat_message(content)
+
     def connect(self):
         self.room_name = self.scope['url_route']['kwargs']['room_name']
         print("The room is {}".format(self.room_name))
@@ -38,23 +87,30 @@ class ChatConsumer(WebsocketConsumer):
 
     # Receive message from WebSocket
     def receive(self, text_data):
-        text_data_json = json.loads(text_data)
-        message = text_data_json['message']
+        data = json.loads(text_data)
+        # self.commands[data['command']](self, data)
+        if data['command'] == 'fetch_messages':
+            self.fetch_messages(data)
+        elif data['command'] == 'new_message':
+            self.new_message(data)
 
+    def send_chat_message(self, message):
         # Send message to room group
         async_to_sync(self.channel_layer.group_send)(
             self.room_group_name,
             {
-                'type': 'chat_message',
+                'type': 'chat_message',#method to be called when message is added to the group
                 'message': message
             }
         )
+
+    def send_message(self, message):
+        self.send(text_data=json.dumps(message))
+
 
     # Receive message from room group
     def chat_message(self, event):
         message = event['message']
 
         # Send message to WebSocket
-        self.send(text_data=json.dumps({
-            'message': message
-        }))
+        self.send(text_data=json.dumps(message))
--- chat/routing.py
+++ chat/routing.py
@@ -1,5 +1,4 @@
 from django.urls import re_path
-
 from chat.consumers import ChatConsumer
 
 websocket_urlpatterns = [
